71d913ff0217a9d31c2721869f57096c36d246e4
илья михайлов <ps.iria@yandex.ru>
2021-01-11 22:39:35 +0700

    add url users

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from api import views
+
 router = DefaultRouter()
+router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    pass

b9b1453e4674168cff547bf64bd7f00ceb979bba
drpsychps <68935465+drpsychps@users.noreply.github.com>
2021-01-12 10:44:11 +0700

    add basename

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 router = DefaultRouter()
-router.register('users', views.UserViewSet)
+router.register('users', views.UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(router.urls)),

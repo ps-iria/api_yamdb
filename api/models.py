098a449481d4b6ef6d366c097820e1fce7a565d6
drpsychps <68935465+drpsychps@users.noreply.github.com>
2021-01-12 11:36:34 +0700

    add model User

--- api/models.py
+++ api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class UserRoles(models.TextChoices):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+
+class User(AbstractUser):
+    role = models.CharField(
+        max_length=10,
+        choices=UserRoles.choices,
+        default=UserRoles.USER,
+        verbose_name='Роль пользователя',
+    )
+    email = models.EmailField(
+        max_length=128,
+        blank=False,
+        unique=True
+    )
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        default=email,
+        blank=True,
+        verbose_name='Логин',
+    )
+    # confirmation_code = models.CharField(
+    #     max_length=255,
+    #     unique=True
+    # )
+    first_name = models.CharField(
+        max_length=15,
+        blank=True,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=15,
+        blank=True,
+        verbose_name='Фамилия',
+    )
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+        verbose_name='О себе',
+    )

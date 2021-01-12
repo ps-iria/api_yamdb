ce5d44af3adf017a3f4e0876860526de637a0902
Александр Краснобаев <alex16654@yandex.ru>
2021-01-12 13:56:19 +0300

    proba branch

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 router = DefaultRouter()
-router.register('users', views.UserViewSet)
+#router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    pass
+# from rest_framework import viewsets
+#
+#
+# class UserViewSet(viewsets.ModelViewSet):
+#     pass

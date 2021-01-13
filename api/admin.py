fe13935a30bb63b04d01b9dceadea8040b6ae331
илья михайлов <ps.iria@yandex.ru>
2021-01-13 23:56:40 +0700

    add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'role',
+        'email',
+        'confirmation_code',
+        'bio',
+        'first_name',
+        'last_name'
+    )
+    search_fields = ("last_name",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(User, UserAdmin)

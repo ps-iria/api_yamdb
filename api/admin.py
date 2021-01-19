d61df0b695cf34ba32e798155c7f184e7dd16c4f
drpsychps <68935465+drpsychps@users.noreply.github.com>
2021-01-19 14:32:24 +0700

    rev2.1

--- api/admin.py
+++ api/admin.py
@@ -43,7 +43,7 @@ class UserAdmin(admin.ModelAdmin):
         'first_name',
         'last_name'
     )
-    search_fields = ("last_name",)
+    search_fields = ('last_name',)
     empty_value_display = "-пусто-"
 
 
